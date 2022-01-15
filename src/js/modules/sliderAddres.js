9e7b8e1ef423ebee0be83b6da120b97e4615f1d3
TatiynaLN <tania.cis2011@yandex.ru>
2022-01-15 13:21:50 +0300

    slider правки

--- src/js/modules/sliderAddres.js
+++ src/js/modules/sliderAddres.js
@@ -4,117 +4,96 @@ export default function slider() {
     if (document.querySelector('.sliders-addres')) {
         const swiperLicenses = new Swiper('.slider-addres', {
 
-            // стрелки
+            // // // Переключение слайда при клике на него
+            // // slideToClickedSlide: true,
+
+            // // //     // // управление клавиатурой 
+            // // //     // keyboard: {
+            // // //     //     // вкл/ выкл
+            // // //     //     enabled: true,
+            // // //     //     // вкл/ выкл только когда слайдер в пределах вьюпорта
+            // // //     //     onlyInViewport: true,
+            // // //     //     // Вкл/выкл управление клавишами pageUp, pageDown
+            // // //     //     pageUpDown: true,
+            // // //     // },
+            // // //     // управление колесом мыши
+            // // mousewheel: {
+            // //     //         //чувствительность колеса
+            // //     sensitivity: 1,
+            // //     //         // класс обьекта на катором будет срабатывать прокрутка мышью
+            // //     eventsTarget: '.swiper-slide-arrow'
+            // // },
+
+            // // //     // скорость прокрутки 
+            // // //     // speed: 800,
+
+            // // // отключение функционала если слайдов меньше чем надо
+            // // // watchOverflow: true,
+
+            // // // отступы между слайдов
+            // // spaceBetween: 113,
+
+
+
+
+            // loopedSlides: 1,
+
+            // // //автовысота
+            // // //     autoHeight: true,
+
+
+            // // //     breakpoints: {
+            // // //       320: {
+
+
+
+            // //колличество пролистываемых слайдов 
+            // slidesPerGroup: 1,
+            // breakpoints: {
+            //     //1440: {
+            //     //         slidesPerView: 2,
+            //     //         loopedSlides: 1
+            //     //       },
+            // }
+            // //         loopedSlides: 1,
+            // //         spaceBetween: 30
+            // //       },
+            // //       1440: {
+            // //         slidesPerView: 2,
+            // //         loopedSlides: 1
+            // //       },
+            // //     }
+            // // стрелки
             navigation: {
-                nextEl: '.swiper-button-next',
-                //prevEl: '.swiper-button-prev'
+                nextEl: '.swiper-button-next'
             },
-
-
-            //     // // буллеты -точки
-            //     // pagination: {
-            //     //     el: '.swiper-pagination',
-
-            //     //     // Прогрессбар - скрол полоса прокрытки сверху
-            //     //     // type: 'progressbar',
-
-
-            //     //     // если подключен Прогрессбар - скрол все что ниже удалить, если надо вывести вместе с точками вернись к видео
-            //     //     clickable: true,
-            //     //     // Динамические буллеты
-            //     //     dynamicBullets: true, // будут изменять свой размер в зависимость от растояния от активной точки
-            //     //     // Кастомные буллеты
-            //     //     renderBullet: function (index, className) {
-            //     //         return '<span class="' + className + '">' + (index + 1) + '</span>';
-            //     //     },
-            //     // },
-
-
-            //     // // SCROLLBAR - САМЫЙ НАСТОЯЩИЙ СКРОЛ
-            //     // scrollbar: {
-            //     //     el: '.swiper-scrollbar',
-            //     //     // возможность перетаскивать скролл
-            //     //     draggable: true,
-            //     // },
-
-            //     // Переключение слайда при клике на него
-            slideToClickedSlide: true,
-
-            //     // // управление клавиатурой 
-            //     // keyboard: {
-            //     //     // вкл/ выкл
-            //     //     enabled: true,
-            //     //     // вкл/ выкл только когда слайдер в пределах вьюпорта
-            //     //     onlyInViewport: true,
-            //     //     // Вкл/выкл управление клавишами pageUp, pageDown
-            //     //     pageUpDown: true,
-            //     // },
-            //     // управление колесом мыши
-            mousewheel: {
-                //         //чувствительность колеса
-                sensitivity: 1,
-                //         // класс обьекта на катором будет срабатывать прокрутка мышью
-                eventsTarget: '.swiper-slide-arrow'
-            },
-
-            //     // авто прокрутка
-            //     // autoplay: {
-            //     //     // пауза между прокруткой
-            //     //     delay: 1000,
-            //     //     // закончить на последнем слайде при отключенной бесконечности
-            //     //     stopOnLastSlide: true,
-            //     //     // отключить после ручного переключения
-            //     //     disableOnInteraction: false
-            //     // },
-
-            //     // скорость прокрутки 
-            //     // speed: 800,
-
-            //     // отключение функционала если слайдов меньше чем надо
-            watchOverflow: true,
-
-            //     // отступы между слайдов
-            spaceBetween: 113,
-
-
             // активный слайд по центру
-            centeredSlides: false,
-
+            //centeredSlides: true,
             // стартовый слайд
-            initialSlide: 0, // отсчет с 0
+            initialSlide: 1, // отсчет с 0
 
             // бесконечный слайд
-            //loop: true,  // скрол и мультирятность отключить, slidesPerView: auto(нельзя),
-
-            //     loopedSlides: 1,
-
-            //автовысота
-            //     autoHeight: true,
-
-
-            //     breakpoints: {
-            //       320: {
-
+            loop: true,  // скрол и мультирятность отключить, slidesPerView: auto(нельзя),
             // колличество слайдов для показа
-            slidesPerView: 1.4, // если значение авто не забудь про css
-
-
-            //колличество пролистываемых слайдов 
-            slidesPerGroup: 1,
+            slidesPerView: 1, // если значение авто не забудь про css
+            // отступы между слайдов
+            spaceBetween: 30,
             breakpoints: {
-                //1440: {
-                //         slidesPerView: 2,
-                //         loopedSlides: 1
-                //       },
+                768: {
+                    // колличество слайдов для показа
+                    slidesPerView: 1.6,
+                },
+                1024: {
+                    // колличество слайдов для показа
+                    slidesPerView: 1.6,
+                    spaceBetween: 50,
+                },
+                1600: {
+                    // колличество слайдов для показа
+                    slidesPerView: 1.6,
+                    spaceBetween: 113,
+                },
             }
-            //         loopedSlides: 1,
-            //         spaceBetween: 30
-            //       },
-            //       1440: {
-            //         slidesPerView: 2,
-            //         loopedSlides: 1
-            //       },
-            //     }
         });
     }
 
